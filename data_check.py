a87b345ab75f1765dc49faef4d2a72fb48ddec34
yassin2206839 <yassin2206839@miuegypt.edu.eg>
2026-01-31 22:45:17 +0200

    Update data_check.py

--- data_check.py
+++ data_check.py
@@ -1,1 +1,9 @@
+import os, glob
+
+print("Labeled images:", len(glob.glob(LABELED_DIR + "/*")))
+print("Unlabeled images:", len(glob.glob(UNLABELED_DIR + "/*")))
+print("Landmark files:", len(glob.glob(LANDMARK_DIR + "/*")))
+
+print("\nSample labeled files:", glob.glob(LABELED_DIR + "/*")[:5])
+print("Sample landmark files:", glob.glob(LANDMARK_DIR + "/*")[:5])
 
