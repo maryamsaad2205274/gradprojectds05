d8199ffe2eb29981ae1a6e44a7684abf45d97efe
yassin2206839 <yassin2206839@miuegypt.edu.eg>
2026-01-31 22:34:45 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+from google.colab import drive
+drive.mount('/content/drive')
+LABELED_DIR   = "/content/drive/MyDrive/projectdataset/images"
+UNLABELED_DIR = "/content/drive/MyDrive/projectdataset/unlabeled"
+LANDMARK_DIR  = "/content/drive/MyDrive/projectdataset/landmarks" 
